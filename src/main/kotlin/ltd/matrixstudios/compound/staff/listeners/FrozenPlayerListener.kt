5766edece66ba64f7c6eef89d1dce7ae51dde725
98ping <67809373+98ping@users.noreply.github.com>
2022-07-10 22:49:10 -0700

    try and fix freezing

--- src/main/kotlin/ltd/matrixstudios/compound/staff/listeners/FrozenPlayerListener.kt
+++ src/main/kotlin/ltd/matrixstudios/compound/staff/listeners/FrozenPlayerListener.kt
@@ -1,12 +1,13 @@
 package ltd.matrixstudios.compound.staff.listeners
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
 
 class FrozenPlayerListener : Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     fun move(e: PlayerMoveEvent) {
         val player = e.player
 
